89dd183c31a7c9f896f159346fb6e83da8604c14
abhijithabhiakl <abhiabhijith367@gmail.com>
2023-12-14 02:48:49 +0530

    og image size change

--- ogpre.js
+++ ogpre.js
@@ -13,7 +13,7 @@ async function generateOGImage() {
     await page.goto(websiteUrl, { waitUntil: 'domcontentloaded' });
 
     // Set the viewport size to the desired image dimensions
-    await page.setViewport({ width: 1200, height: 630 });
+    await page.setViewport({ width: 1200, height: 730 });
 
     // Capture a screenshot of the rendered HTML
     await page.screenshot({ path: './img/og_image.png' });
