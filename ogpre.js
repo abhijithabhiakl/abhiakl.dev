a218188ca2341267dd87b5f09683d698f872634d
abhijithabhiakl <abhiabhijith367@gmail.com>
2023-12-14 00:39:09 +0530

    web preview og

--- ogpre.js
+++ ogpre.js
@@ -1,17 +1,19 @@
-// Example script using Puppeteer to generate an Open Graph image
-
 const puppeteer = require('puppeteer');
 
 async function generateOGImage() {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setContent('<html content>'); // Replace with your HTML content
+
+    const websiteUrl = 'https://abhiakl.xyz';
+
+    // Navigate to your website
+    await page.goto(websiteUrl, { waitUntil: 'domcontentloaded' });
 
     // Set the viewport size to the desired image dimensions
     await page.setViewport({ width: 1200, height: 630 });
 
     // Capture a screenshot of the rendered HTML
-    await page.screenshot({ path: './img/og_image.png' });
+    await page.screenshot({ path: 'og_image.png' });
 
     await browser.close();
 }
