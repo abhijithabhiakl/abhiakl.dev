b143870f74871fe1b90033316071c91ab23e4ad5
Abhijithabhi <abhiabhijith367@gmail.com>
2026-01-16 01:15:57 +0530

    Fix footer links and update RSS feed URL

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,9 @@ export default function Footer() {
             <a href="https://twitter.com/abhijithabhiakl" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-x-twitter ico ico1 fa-3x"></i></a>
             <a href="mailto:hi@abhiakl.xyz"><i className="fa-solid fa-envelope ico ico2 fa-3x"></i></a>
             <a href="https://github.com/abhijithabhiakl" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github ico ico3 fa-3x"></i></a>
-            <a href="https://blog.abhijithakl.xyz/rss.xml" target="_blank" rel="noopener noreferrer"><i className="fa-solid fa-square-rss ico ico4 fa-3x"></i></a>
+            <a href="https://blog.abhidev.in/rss.xml" target="_blank" rel="noopener noreferrer"><i className="fa-solid fa-square-rss ico ico4 fa-3x"></i></a>
             <p className="copyright">Made with literal caveman tech by &copy; abhiakl 2025</p>
         </footer>
     );
 }
+
